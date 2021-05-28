c69aaddc7b02c6833b516898db1054eec2207975
chandra-zs <chandrasekhar@zelarsoft.com>
2021-05-28 16:00:34 +0530

    first commit

--- seed.groovy
+++ seed.groovy
@@ -2,7 +2,7 @@ folder('CI-Pipelines') {
     displayName('CI Pipelines')
     description('CI Pipelines')
 }
-pipelineJob('CI-Pipeline/frontend') {
+pipelineJob('CI-Pipelines/frontend') {
     configure { flowdefinition ->
         flowdefinition << delegate. 'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
             'scm'(class:'hudson.plugin.git.GitSCM',plugin:'git') {
