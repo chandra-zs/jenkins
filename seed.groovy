d9bb70b9cc89263814fa57fe554737b17aa9b173
chandra-zs <chandrasekhar@zelarsoft.com>
2021-07-03 16:08:36 +0530

    first commit

--- seed.groovy
+++ seed.groovy
@@ -2,7 +2,9 @@ folder('CI-Pipelines') {
     displayName('CI Pipelines')
     description('CI Pipelines')
 }
+
 def component = ["frontend","users","login","todo"];
+
 def count=(component.size()-1)
 for (i in 0..count) {
     def j=component[i]
@@ -12,7 +14,7 @@ for (i in 0..count) {
                 'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
                     'userRemoteConfigs' {
                         'hudson.plugins.git.UserRemoteConfig' {
-                            'url'('https://github.com/chandra-zs/'+j+'.git')
+                            'url'('https://github.com/srikavyapendiala/'+j+'.git')
                             'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
                         }
                     }
@@ -25,6 +27,27 @@ for (i in 0..count) {
                 'scriptPath'('Jenkinsfile-Docker')
                 'lightweight'(true)
             }
+        }
+    }
+}
+
+pipelineJob("Deployment Pipeline") {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/chandra-zs/jenkins.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/tags/*')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile-Deployment')
+            'lightweight'(true)
         }
     }
 }
