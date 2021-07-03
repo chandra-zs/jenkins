541c08cfe57a838cbdac1da8c837c4bd78ac78ff
chandra-zs <chandrasekhar@zelarsoft.com>
2021-07-03 17:10:30 +0530

    first commit

--- seed.groovy
+++ seed.groovy
@@ -31,23 +31,3 @@ for (i in 0..count) {
     }
 }
 
-pipelineJob("Deployment Pipeline") {
-    configure { flowdefinition ->
-        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-                'userRemoteConfigs' {
-                    'hudson.plugins.git.UserRemoteConfig' {
-                        'url'('https://github.com/chandra-zs/jenkins.git')
-                    }
-                }
-                'branches' {
-                    'hudson.plugins.git.BranchSpec' {
-                        'name'('main')
-                    }
-                }
-            }
-            'scriptPath'('Jenkinsfile-Deployment')
-            'lightweight'(true)
-        }
-    }
-}
