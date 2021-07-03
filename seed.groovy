27926e5c279c85adbe91d778938c7e750db4f3a1
chandra-zs <chandrasekhar@zelarsoft.com>
2021-07-03 15:47:17 +0530

    first commit

--- seed.groovy
+++ seed.groovy
@@ -22,7 +22,7 @@ for (i in 0..count) {
                         }
                     }
                 }
-                'scriptPath'('Jenkinsfile')
+                'scriptPath'('Jenkinsfile-Docker')
                 'lightweight'(true)
             }
         }
