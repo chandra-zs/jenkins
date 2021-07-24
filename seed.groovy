4529e70c6d613115f1ddfdde13d19aa0161bbeae
chandra-zs <chandrasekhar@zelarsoft.com>
2021-07-24 23:16:07 +0530

    first commit

--- seed.groovy
+++ seed.groovy
@@ -1,33 +1,81 @@
 folder('CI-Pipelines') {
-  displayName('CI Pipelines')
-  description('CI Pipelines')
+    displayName('CI Pipelines')
+    description('CI Pipelines')
 }
 
 def component = ["frontend","users","login","todo"];
 
 def count=(component.size()-1)
 for (i in 0..count) {
-  def j=component[i]
-  pipelineJob("CI-Pipelines/${j}-ci") {
-    configure { flowdefinition ->
-      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-          'userRemoteConfigs' {
-            'hudson.plugins.git.UserRemoteConfig' {
-              'url'('https://github.com/chandra-zs/'+j+'.git')
-              'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+    def j=component[i]
+    pipelineJob("CI-Pipelines/${j}-ci") {
+        configure { flowdefinition ->
+            flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
+                'triggers' {
+                    'hudson.triggers.SCMTrigger' {
+                        'spec'('*/2 * * * 1-5')
+                        'ignorePostCommitHooks'(false)
+                    }
+                }
             }
-          }
-          'branches' {
-            'hudson.plugins.git.BranchSpec' {
-               'name'('*/tags/*')
+            flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+                'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                    'userRemoteConfigs' {
+                        'hudson.plugins.git.UserRemoteConfig' {
+                            'url'('https://github.com/chandra-zs/'+j+'.git')
+                            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+                        }
+                    }
+                    'branches' {
+                        'hudson.plugins.git.BranchSpec' {
+                            'name'('*/tags/*')
+                        }
+                    }
+                }
+                'scriptPath'('Jenkinsfile-Docker')
+                'lightweight'(true)
             }
-          }
         }
-        'scriptPath'('Jenkinsfile')
-        'lightweight'(true)
-      }
     }
-  }
+}
+pipelineJob("Deployment Pipeline") {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/zsdevops01/jenkins.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile-Deployment')
+            'lightweight'(true)
+        }
+    }
 }
 
+pipelineJob("Docker Pipeline") {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/zsdevops01/jenkins.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/tags/*')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile-Docker')
+            'lightweight'(true)
+        }
+    }
+}
