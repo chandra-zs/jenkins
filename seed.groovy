e9830891fd139a47a2d650c8944c0eccd800f620
chandra-zs <chandrasekhar@zelarsoft.com>
2021-08-05 11:19:23 +0530

    first commit

--- seed.groovy
+++ seed.groovy
@@ -79,3 +79,40 @@ pipelineJob("Docker Pipeline") {
         }
     }
 }
+folder('CI-Pipelines') {
+    displayName('CI Pipelines-Docker')
+    description('CI Pipelines-Docker')
+}
+
+for (i in 0..count) {
+    def j=component[i]
+    pipelineJob("CI-Pipelines-Docker/${j}-ci") {
+        configure { flowdefinition ->
+            flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
+                'triggers' {
+                    'hudson.triggers.SCMTrigger' {
+                        'spec'('*/2 * * * 1-5')
+                        'ignorePostCommitHooks'(false)
+                    }
+                }
+            }
+            flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+                'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                    'userRemoteConfigs' {
+                        'hudson.plugins.git.UserRemoteConfig' {
+                            'url'('https://github.com/chandra-zs/'+j+'.git')
+                            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+                        }
+                    }
+                    'branches' {
+                        'hudson.plugins.git.BranchSpec' {
+                            'name'('*/tags/*')
+                        }
+                    }
+                }
+                'scriptPath'('Jenkinsfile-Docker')
+                'lightweight'(true)
+            }
+        }
+    }
+}
