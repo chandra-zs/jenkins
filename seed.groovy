aba8e45290f1482be024f9c2e5d5d07b89dedcf4
chandra-zs <chandrasekhar@zelarsoft.com>
2021-08-05 11:35:38 +0530

    seed job for docker

--- seed.groovy
+++ seed.groovy
@@ -60,15 +60,13 @@ pipelineJob("Deployment Pipeline") {
 }
 
 
-folder('CI-Pipelines') {
+folder('CI-Pipelines-Docker') {
     displayName('CI Pipelines-Docker')
     description('CI Pipelines-Docker')
 }
-def Component = ["frontend","users","login","todo"];
 
-def Count=(Component.size()-1)
-for (i in 0..Count) {
-    def j=Component[i]
+for (i in 0..count) {
+    def j=component[i]
     pipelineJob("CI-Pipelines-Docker/${j}-ci") {
         configure { flowdefinition ->
             flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
